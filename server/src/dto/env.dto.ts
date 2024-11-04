b147364650988cf035fd24730556d5e8b850c2a8
Micah Effiong <micaiah.effiong@gmail.com>
2024-11-04 21:27:10 +0100

    update env validation code

--- server/src/dto/env.dto.ts
+++ server/src/dto/env.dto.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 
+/**
+ *@description for validation `process.env`
+ */
 export const EnvDto = z.object({
   PORT: z.coerce.number(),
-  JWT_SECRET: z.string()
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string()
 });
 
 export type EnvDto = z.infer<typeof EnvDto>;
