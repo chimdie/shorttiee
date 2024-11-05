983ebb48f350e459eaeb9c298b3694a1407edb19
sammy-code98 <sammysmart98@yahoo.com>
2024-11-05 23:29:02 +0100

    update colors in tailwind config

--- host/tailwind.config.js
+++ host/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1F253F",
+        secondary: "#1D3D3A",
+        purple: "#3F1F3A",
+        yellow_dark: "#B1AD4E",
+        yellow_light: "#EAE68B",
+        grey_100: "#F7F6F2",
+        grey_200: "#EBEBEB",
+        grey_300: "#A1A3B0",
+        grey_400: "#6B7280",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
-}
+};
