c1956dc9fbf61e7581a110224f05a352f9611e1c
Micah Effiong <micaiah.effiong@gmail.com>
2024-11-07 00:13:52 +0100

    create db queries for user and auth tables

--- server/src/db/users.db.ts
+++ server/src/db/users.db.ts
@@ -1,6 +1,7 @@
+import { Database } from "better-sqlite3";
 import { WithDBTimestamps } from "../types/utils";
 
-export type User = WithDBTimestamps<{
+export type User = {
   id: string;
   firstName: string;
   lastName: string;
@@ -9,10 +10,28 @@ export type User = WithDBTimestamps<{
   businessName?: string;
   referrerCode?: string;
   address?: string;
-}>;
+};
 
-export type Auth = WithDBTimestamps<{
-  id: string;
-  hash: string;
-  userId: string;
-}>;
+export function createUser(db: Database) {
+  return db.prepare<Array<User>>(
+    "INSERT INTO tblUsers(id, firstName, lastName, email, mobileNumber, businessName, referrerCode, address) VALUES(@id, @firstName, @lastName, @email, @mobileNumber, @businessName, @referrerCode, @address)"
+  );
+}
+
+export function findUserById(db: Database, id: string) {
+  return db
+    .prepare<
+      string[],
+      WithDBTimestamps<User>
+    >("SELECT * FROM tblUsers WHERE id = ?")
+    .get(id);
+}
+
+export function findUserByEmail(db: Database, email: string) {
+  return db
+    .prepare<
+      string[],
+      WithDBTimestamps<User>
+    >("SELECT * FROM tblUsers WHERE email = ?")
+    .get(email);
+}
