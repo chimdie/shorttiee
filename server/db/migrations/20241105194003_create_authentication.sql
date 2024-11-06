4d7c83154d77193496738b90cdd01cf7066d9a87
Micah Effiong <micaiah.effiong@gmail.com>
2024-11-07 00:12:35 +0100

    updated database trigger and fix foreignKey refs

--- server/db/migrations/20241105194003_create_authentication.sql
+++ server/db/migrations/20241105194003_create_authentication.sql
@@ -2,17 +2,19 @@
 CREATE TABLE IF NOT EXISTS tblAuthentications (
   id VARCHAR(36) PRIMARY KEY NOT NULL, -- use uuid
   hash VARCHAR(255) NOT NULL,
-  userId FOREIGN KEY REFERENCES tblUsers(id) ON DELETE CASCADE,
+  userId VARCHAR(36) UNIQUE NOT NULL,
 
   createdAt DATETIME DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
-  updatedAt DATETIME DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW'))
+  updatedAt DATETIME DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
+  
+  FOREIGN KEY (userId) REFERENCES tblUsers(id) ON DELETE CASCADE
 );
 
-CREATE TRIGGER IF NOT EXISTS trgUpdatedAt AFTER UPDATE ON tblAuthentications 
+CREATE TRIGGER IF NOT EXISTS trgAuthenticationsUpdatedAt AFTER UPDATE ON tblAuthentications 
 BEGIN 
   UPDATE tblAuthentications SET updatedAt=(STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')) WHERE id=OLD.id;
 END;
 
 -- migrate:down
-DROP TRIGGER IF EXISTS trgUpdatedAt;
+DROP TRIGGER IF EXISTS trgAuthenticationsUpdatedAt;
 DROP TABLE IF EXISTS tblAuthentications;
