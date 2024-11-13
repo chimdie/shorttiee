b31c38a2b387643a51ef98b1a56466268ebda40c
Micah Effiong <micaiah.effiong@gmail.com>
2024-11-13 20:23:31 +0100

    implement otp verificaton with ttl

--- server/src/utils/otp.ts
+++ server/src/utils/otp.ts
@@ -1,6 +1,7 @@
 import nodeCrypto from "crypto";
+import { appEnv } from "./config-env";
 
-function generateOtp(size: number = 6) {
+function generateOtp(size: number = Number(appEnv.OTP_SIZE)) {
   const otp = Array.from({ length: size })
     .map((_) => nodeCrypto.randomInt(10))
     .join("");
@@ -9,7 +10,11 @@ function generateOtp(size: number = 6) {
   return [otp, hash] as const;
 }
 
-function verifyOtp(otp: string, hash: string) {
+function verifyOtp(otp: string, hash: string, otpTTL: Date) {
+  if (otpTTL.getTime() < Date.now()) {
+    return false;
+  }
+
   const otpHash = nodeCrypto.createHash("sha1").update(otp).digest("hex");
   return otpHash === hash;
 }
