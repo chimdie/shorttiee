d5cfcc3c39a90046a372a007e935de7ad030ef6f
sammy-code98 <sammysmart98@yahoo.com>
2024-11-15 23:17:09 +0100

    feat:shortlet empty state

--- host/src/pages/Dashboard/Shortlet/index.tsx
+++ host/src/pages/Dashboard/Shortlet/index.tsx
@@ -1,3 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Hotel } from "lucide-react";
+
 export default function Shortlet(): JSX.Element {
-  return <div>Shortlet Page</div>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center mx-auto h-full">
+        <div className="p-4 border rounded-md ">
+          <Hotel className="size-12" />
+        </div>
+        <div className="py-8 md:w-1/2">
+          <div className="space-y-4">
+            <h3 className="text-shorttiee_primary text-lg md:text-xl font-medium text-center">
+              You currently dont have any Shortlets Listed
+            </h3>
+            <p className="text-grey_300 text-sm md:text-base text-center">
+              Looks like you haven't added any shortlets yet.This area will light up with your
+              shortlets once you list or add them{" "}
+            </p>
+          </div>
+        </div>
+        <div>
+          <Button className="bg-shorttiee_primary font-medium w-full">Add Shortlet</Button>
+        </div>
+      </div>
+    </>
+  );
 }
