78e017d0075d5cb5b90ecf87c4319c5d89767553
sammy-code98 <sammysmart98@yahoo.com>
2024-11-19 00:13:45 +0100

    feat:delete shorlet modal

--- host/src/components/Shortlet/action-popover.tsx
+++ host/src/components/Shortlet/action-popover.tsx
@@ -1,17 +1,32 @@
-import { DashboardRoutes } from "@/types/routes";
-import { Divider, Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
-import { EllipsisVertical } from "lucide-react";
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Divider, Popover, PopoverContent, PopoverTrigger, useDisclosure } from "@nextui-org/react";
+import { DashboardRoutes } from "@/types/routes";
+import { EllipsisVertical } from "lucide-react";
+import DeleteShortletModal from "./delete-shortlet-modal";
 
 type ActionPopoverProps = {
   id: string;
 };
 
 export default function ActionPopover({ id }: ActionPopoverProps): JSX.Element {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const deleteShortletModal = useDisclosure();
+
+  const openDeleteShortlet = (): void => {
+    setIsOpen(false);
+    deleteShortletModal.onOpen();
+  };
+
   return (
     <>
-      <Popover placement="bottom-end" backdrop="opaque" size="sm">
+      <Popover
+        placement="bottom-end"
+        size="sm"
+        isOpen={isOpen}
+        onOpenChange={(open) => setIsOpen(open)}
+      >
         <PopoverTrigger>
           <div>
             <EllipsisVertical className="text-xs md:text-sm text-black cursor-pointer" />
@@ -25,12 +40,19 @@ export default function ActionPopover({ id }: ActionPopoverProps): JSX.Element {
               </div>
             </Link>
             <Divider orientation="horizontal" />
-            <div className="py-3 pl-3 pr-6 cursor-pointer">
+            <div className="py-3 pl-3 pr-6 cursor-pointer" onClick={openDeleteShortlet}>
               <p className="text-sm font-medium">Delete Shortlet</p>
             </div>
           </div>
         </PopoverContent>
       </Popover>
+
+      <DeleteShortletModal
+        isOpen={deleteShortletModal.isOpen}
+        onClose={deleteShortletModal.onClose}
+        onOpenChange={deleteShortletModal.onOpenChange}
+        id={id}
+      />
     </>
   );
 }
