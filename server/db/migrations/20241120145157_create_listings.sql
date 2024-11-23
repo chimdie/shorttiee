dab327abb1126d67e3176704dc0963f03fcf97e7
Micah Effiong <micaiah.effiong@gmail.com>
2024-11-23 21:55:57 +0100

    added category and listing table

--- server/db/migrations/20241120145157_create_listings.sql
+++ server/db/migrations/20241120145157_create_listings.sql
@@ -1,13 +1,4 @@
 -- migrate:up
-create table tblCategories (
-  id VARCHAR(36) PRIMARY KEY NOT NULL, -- use uuid
-  name varchar(50) NOT NULL UNIQUE,
-  comment TEXT,
-
-  createdAt DATETIME DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
-  updatedAt DATETIME DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW'))
-);
-
 CREATE TABLE tblListings (
   id VARCHAR(36) PRIMARY KEY NOT NULL, -- use uuid
   name varchar(50) NOT NULL,
@@ -15,7 +6,6 @@ CREATE TABLE tblListings (
   -- location -- coordinate
   type TEXT CHECK(type IN ("SHORTLET","RENTAL","SALE")) NOT NULL,
   status TEXT CHECK(status IN ("AWAITING_REVIEW","REJECTED","APPROVED")) NOT NULL,
-  details TEXT,
   description TEXT,
   price INT,
   rate INT,
@@ -32,11 +22,6 @@ CREATE TABLE tblListings (
   FOREIGN KEY (categoryId) REFERENCES tblCategories(id) ON DELETE CASCADE
 );
 
-CREATE TRIGGER IF NOT EXISTS trgCategoriesUpdatedAt AFTER UPDATE ON tblCategories 
-BEGIN 
-  UPDATE tblCategories SET updatedAt=(STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')) WHERE id=OLD.id;
-END;
-
 CREATE TRIGGER IF NOT EXISTS trgListingsUpdatedAt AFTER UPDATE ON tblListings
 BEGIN 
   UPDATE tblListings SET updatedAt=(STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')) WHERE id=OLD.id;
@@ -44,6 +29,4 @@ END;
 
 -- migrate:down
 DROP TRIGGER IF EXISTS trgListingsUpdatedAt;
-DROP TRIGGER IF EXISTS trgCategoriesUpdatedAt;
 DROP TABLE IF EXISTS tblListings;
-DROP TABLE IF EXISTS tblCategories;
---
+++ server/db/migrations/20241123140022_create_categories.sql
@@ -0,0 +1,18 @@
+-- migrate:up
+create table tblCategories (
+  id VARCHAR(36) PRIMARY KEY NOT NULL, -- use uuid
+  name varchar(50) NOT NULL UNIQUE,
+  comment TEXT,
+
+  createdAt DATETIME DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
+  updatedAt DATETIME DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW'))
+);
+
+CREATE TRIGGER IF NOT EXISTS trgCategoriesUpdatedAt AFTER UPDATE ON tblCategories 
+BEGIN 
+  UPDATE tblCategories SET updatedAt=(STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW')) WHERE id=OLD.id;
+END;
+
+-- migrate:down
+DROP TRIGGER IF EXISTS trgCategoriesUpdatedAt;
+DROP TABLE IF EXISTS tblCategories;
