af945a4d70ef505737ae985f47f90bc136a6cbda
Micah Effiong <micaiah.effiong@gmail.com>
2024-11-23 22:12:52 +0100

    use services from app locals

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
 import { app } from "./app";
 import { appEnv } from "./config/env.config";
+import { OTP } from "./utils/otp";
+
+app.locals.otp = OTP;
 
 app.listen(appEnv.PORT ?? 4000);
--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -4,10 +4,17 @@ import { WithDBTimestamps } from "./utils";
 
 declare global {
   namespace NodeJS {
-    export interface ProcessEnv extends EnvDto { }
+    export interface ProcessEnv extends EnvDto {}
   }
 
   namespace Express {
+    interface Locals {
+      otp: {
+        generateOtp: (size?: number) => readonly [string, string];
+        verifyOtp: (otp: string, hash: string, otpTTL: Date) => boolean;
+        hashOtp: (payload: string) => string;
+      };
+    }
     interface Request {
       user?: WithDBTimestamps<User>;
     }
