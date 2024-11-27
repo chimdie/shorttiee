32393aae32b4b15411306bd9da47fa312761b418
sammy-code98 <sammysmart98@yahoo.com>
2024-11-27 23:26:07 +0100

    feat:add details section in shorlet details page

--- host/src/pages/Dashboard/Shortlet/shortletDetails.tsx
+++ host/src/pages/Dashboard/Shortlet/shortletDetails.tsx
@@ -1,6 +1,28 @@
 import { DashboardRoutes } from "@/types/routes";
-import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
+import { BreadcrumbItem, Breadcrumbs, Chip } from "@nextui-org/react";
+import { AirVent, Bath, BedDouble, CookingPot, House, type LucideIcon } from "lucide-react";
 import { Link } from "react-router-dom";
+
+type DetailCard = {
+  icon: LucideIcon;
+  text: string;
+};
+
+const detailsArr: DetailCard[] = [
+  { icon: House, text: "Duplex" },
+  { icon: BedDouble, text: "1 Bedroom" },
+  { icon: Bath, text: "1 Bathroom" },
+  { icon: CookingPot, text: "2 Kitchen" },
+  { icon: AirVent, text: "Airvents" },
+];
+const DetailCard = ({ icon: Icon, text }: DetailCard) => {
+  return (
+    <div className="border border-grey_300 p-4 rounded-md shadow-sm flex flex-col items-center justify-center space-y-2 min-w-28 h-28">
+      <Icon />
+      <p>{text}</p>
+    </div>
+  );
+};
 
 export default function ShortletDetails() {
   return (
@@ -11,7 +33,56 @@ export default function ShortletDetails() {
         </BreadcrumbItem>
         <BreadcrumbItem>Shortlet name</BreadcrumbItem>
       </Breadcrumbs>
-      <div>shortletDetails</div>
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <h2 className="text-shorttiee_primary text-lg font-bold">Apartment Name</h2>
+          <div className="flex gap-4">
+            <p>Duplex</p>
+            <Chip
+              radius="sm"
+              size="sm"
+              className="text-shorttiee_yellow-dark bg-shorttiee_yellow-light text-xs font-normal capitalize"
+            >
+              pending
+            </Chip>
+          </div>
+        </div>
+        <div>images here</div>
+        <div className="space-y-3">
+          <h3 className="text-shorttiee_primary text-md font-bold">Details</h3>
+          <div className="flex gap-4">
+            {detailsArr.map((details, index) => (
+              <DetailCard key={index} icon={details.icon} text={details.text} />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-3">
+          <h3 className="text-shorttiee_primary text-md font-bold">Description</h3>
+          <div className="w-3/4 space-y-2">
+            <p className="text-grey_400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est assumenda voluptate
+              accusamus vitae! Labore aliquid quia consectetur asperiores esse iste fugit at natus
+              consequatur veniam nemo, quos ad! Soluta, obcaecati!
+            </p>
+            <p className="text-grey_400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est assumenda voluptate
+              accusamus vitae! Labore aliquid quia consectetur asperiores esse iste fugit at natus
+              consequatur veniam nemo, quos ad! Soluta, obcaecati!
+            </p>
+            <p className="text-grey_400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est assumenda voluptate
+              accusamus vitae! Labore aliquid quia consectetur asperiores esse iste fugit at natus
+              consequatur veniam nemo, quos ad! Soluta, obcaecati!
+            </p>
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <h3 className="text-shorttiee_primary text-md font-bold">Facilities and Add Ons</h3>
+
+        </div>
+        shortletDetails
+      </div>
     </div>
   );
 }
