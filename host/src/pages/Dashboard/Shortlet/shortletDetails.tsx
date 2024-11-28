b6f02098625a2d456b8a360c60f4d4b4960f06ff
sammy-code98 <sammysmart98@yahoo.com>
2024-11-28 13:56:15 +0100

    clean pr

--- host/src/pages/Dashboard/Shortlet/shortletDetails.tsx
+++ host/src/pages/Dashboard/Shortlet/shortletDetails.tsx
@@ -1,38 +1,3 @@
-// import { DashboardRoutes } from "@/types/routes";
-// import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
-// import { Link } from "react-router-dom";
-
-// export default function ShortletDetails() {
-//   return (
-//     <div className="py-6 space-y-6">
-//       <Breadcrumbs>
-//         <BreadcrumbItem>
-//           <Link to={DashboardRoutes.shortlets}>Shortlets</Link>
-//         </BreadcrumbItem>
-//         <BreadcrumbItem>Shortlet name</BreadcrumbItem>
-//       </Breadcrumbs>
-//       <div>shortletDetails</div>
-//     </div>
-//   );
-// }
-
-// import { DashboardRoutes } from "@/types/routes";
-// import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
-// import { Link } from "react-router-dom";
-
-// export default function ShortletDetails() {
-//   return (
-//     <div className="py-6 space-y-6">
-//       <Breadcrumbs>
-//         <BreadcrumbItem>
-//           <Link to={DashboardRoutes.shortlets}>Shortlets</Link>
-//         </BreadcrumbItem>
-//         <BreadcrumbItem>Shortlet name</BreadcrumbItem>
-//       </Breadcrumbs>
-//       <div>shortletDetails</div>
-//     </div>
-//   );
-// }
 
 import { Link } from "react-router-dom";
 import {
