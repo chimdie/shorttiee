56957dc8a30efb0fe46870237151eec2d4c8fa02
sammy-code98 <sammysmart98@yahoo.com>
2024-12-02 16:05:37 +0100

    feat:account page

--- host/src/pages/Dashboard/Account/index.tsx
+++ host/src/pages/Dashboard/Account/index.tsx
@@ -1,8 +1,20 @@
-import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
+import { useState } from "react";
+import { Avatar, BreadcrumbItem, Breadcrumbs, Button, Input } from "@nextui-org/react";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Link } from "react-router-dom";
 import { DashboardRoutes } from "@/types/routes";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AccountSchema } from "@/schema/account.schema";
+import { Building2, Mail, MapPin, Phone, UserRound } from "lucide-react";
 
 export default function Account(): JSX.Element {
+  const [isEdit, setIsEdit] = useState<boolean>(true)
+
+  const form = useForm<AccountSchema>({
+    resolver: zodResolver(AccountSchema),
+  });
+
   return (
     <div className="py-6 space-y-6">
       <Breadcrumbs>
@@ -12,7 +24,170 @@ export default function Account(): JSX.Element {
         <BreadcrumbItem>Account</BreadcrumbItem>
       </Breadcrumbs>
 
-      <div className="space-y-4">hehehe</div>
+      <div className="flex flex-col justify-center items-center space-y-6 py-4">
+        <div>
+          <Avatar
+            src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+            className="w-40 h-40 text-large"
+          />
+        </div>
+
+        <Form {...form}>
+          <form className="flex flex-col space-y-7">
+            <div className="grid grid-cols-2 space-x-4">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        radius="sm"
+                        variant="bordered"
+                        placeholder="Last Name"
+                        type="text"
+                        defaultValue="Tunde"
+                        startContent={
+                          <UserRound size={16} className="pointer-events-none text-grey_400" />
+                        }
+                        isDisabled={isEdit}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        radius="sm"
+                        variant="bordered"
+                        placeholder="Last Name"
+                        type="text"
+                        defaultValue="Musa"
+                        startContent={
+                          <UserRound size={16} className="pointer-events-none text-grey_400" />
+                        }
+                        isDisabled={isEdit}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      radius="sm"
+                      variant="bordered"
+                      placeholder="Email"
+                      type="email"
+                      defaultValue="tundeeeLargartha@bjorn.ironside"
+                      startContent={
+                        <Mail size={16} className="pointer-events-none text-grey_400" />
+                      }
+                      isDisabled={isEdit}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      radius="sm"
+                      variant="bordered"
+                      placeholder="Phone Number"
+                      type="text"
+                      defaultValue="90923439993"
+                      startContent={
+                        <Phone size={16} className="pointer-events-none text-grey_400" />
+                      }
+                      isDisabled={isEdit}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="home"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      radius="sm"
+                      variant="bordered"
+                      placeholder="Your Home Address"
+                      defaultValue="Alausa quaters ikoyi lagos"
+                      type="text"
+                      startContent={
+                        <MapPin size={16} className="pointer-events-none text-grey_400" />
+                      }
+                      isDisabled={isEdit}
+
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="bussinessName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      radius="sm"
+                      variant="bordered"
+                      placeholder="Business Name (Optional)"
+                      type="text"
+                      startContent={
+                        <Building2 size={16} className="pointer-events-none text-grey_400" />
+                      }
+                      isDisabled={isEdit}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              className="bg-shorttiee_primary text-white font-semibold"
+              radius="sm"
+              onClick={() => setIsEdit(!isEdit)}
+            >
+              {isEdit ? "Edit" : "Save"}
+            </Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
