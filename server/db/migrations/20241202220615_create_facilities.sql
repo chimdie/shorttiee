0575091bf711294dbc2d45012901d230d7c4fe0c
Micah Effiong <micaiah.effiong@gmail.com>
2024-12-04 18:58:30 +0100

    make facility name unique

--- server/db/migrations/20241202220615_create_facilities.sql
+++ server/db/migrations/20241202220615_create_facilities.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 CREATE TABLE tblFacilities (
   id VARCHAR(36) PRIMARY KEY NOT NULL, -- use uuid
-  name varchar(50) NOT NULL,
+  name varchar(50) UNIQUE NOT NULL,
   icon varchar(50) NOT NULL,
   comment TEXT,
   color TEXT,
