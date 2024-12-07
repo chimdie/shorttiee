aeef8108872f3499041ec1d2b7fa614644093aaa
Micah Effiong <micaiah.effiong@gmail.com>
2024-12-07 18:51:27 +0100

    Update docs
    updated the following
    - Added validator url
    - Enabled deep linking
    - Display request duration
    - Disabled "Try It Out"
    - Display Operation Id
    - Persist authorization
    - Change syntax highlight

--- server/src/routes/docs.routes.ts
+++ server/src/routes/docs.routes.ts
@@ -2,5 +2,22 @@ import { Router } from "express";
 import { oapi } from "../config/docs.config";
 
 export const docsRouter = Router();
-
-docsRouter.use(oapi.swaggerui());
+docsRouter.use(
+  oapi.swaggerui({
+    validatorUrl: "http://localhost:3000/api/v1/openapi/validate",
+    deepLinking: true,
+    displayRequestDuration: true,
+    tryItOutEnabled: true,
+    displayOperationId: true,
+    persistAuthorization: true,
+    syntaxHighlight: { activate: true, theme: "arta" }
+    // layout: "StandaloneLayout",
+    // presets: [
+    //   "SwaggerUIBundle.presets.apis" as any,
+    //   "SwaggerUIStandalonePreset" as any
+    // ],
+    // plugins: [
+    //   "SwaggerUIBundle.plugins.DownloadUrl" as any,
+    // ]
+  })
+);
