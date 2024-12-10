9abb105774153072d8b910467feafec8d8b57741
Micah Effiong <micaiah.effiong@gmail.com>
2024-12-10 13:35:57 +0100

    add gracefull shutdown kind

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -3,5 +3,3 @@ import { appEnv } from "./env.config";
 
 const dbPath = appEnv.DATABASE_URL.replace("sqlite3:", "");
 export const db = new Database(dbPath);
-
-process.on("exit", () => db.close());
