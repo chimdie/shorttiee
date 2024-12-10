f6fd37f1a5ec1a4767e23bfafee1ab00c5091e9e
Micah Effiong <micaiah.effiong@gmail.com>
2024-12-10 13:33:25 +0100

    set doc expansion to "none" to keep the docs clean
    ignore the commented code, I'm working on something
    also don't remove it, you'll ruin my day

--- server/src/routes/docs.routes.ts
+++ server/src/routes/docs.routes.ts
@@ -4,20 +4,29 @@ import { oapi } from "../config/docs.config";
 export const docsRouter = Router();
 docsRouter.use(
   oapi.swaggerui({
-    validatorUrl: "http://localhost:3000/api/v1/openapi/validate",
     deepLinking: true,
     displayRequestDuration: true,
     tryItOutEnabled: true,
     displayOperationId: true,
     persistAuthorization: true,
-    syntaxHighlight: { activate: true, theme: "arta" }
+    syntaxHighlight: { activate: true, theme: "arta" },
+    docExpansion: "none"
+
+    // requestSnippetsEnabled: true,
+    // requestSnippets: {
+    //   generators: {
+    //     node_native: {
+    //       title: "NodeJs Native",
+    //       syntax: "javascript"
+    //     }
+    //   }
+    // },
+
     // layout: "StandaloneLayout",
     // presets: [
     //   "SwaggerUIBundle.presets.apis" as any,
     //   "SwaggerUIStandalonePreset" as any
-    // ],
-    // plugins: [
-    //   "SwaggerUIBundle.plugins.DownloadUrl" as any,
     // ]
+    // plugins: ["SwaggerUIBundle.plugins.DownloadUrl" as any]
   })
 );
