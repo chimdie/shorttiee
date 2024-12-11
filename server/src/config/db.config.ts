7fd550681889fb2ed882b343062106914cb948e6
Micah Effiong <micaiah.effiong@gmail.com>
2024-12-11 17:46:42 +0100

    testing WAL mode
    NOT IN USE yet

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -3,3 +3,6 @@ import { appEnv } from "./env.config";
 
 const dbPath = appEnv.DATABASE_URL.replace("sqlite3:", "");
 export const db = new Database(dbPath);
+// db.pragma("journal_mode=WAL");
+//
+// console.log(db.pragma("journal_mode"));
