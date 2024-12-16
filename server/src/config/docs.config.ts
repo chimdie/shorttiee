55d6257a1840be4a24bc18b849deba0fd8004797
Micah Effiong <micaiah.effiong@gmail.com>
2024-12-16 16:03:17 +0100

    update description to include links for both UI's

--- server/src/config/docs.config.ts
+++ server/src/config/docs.config.ts
@@ -1,12 +1,18 @@
 import openapi from "@wesleytodd/openapi";
 
+const description = `
+Shorttiee backend documentation <br><br>
+Swagger API reference <a href="/api/v1/docs">here</a> <br>
+Scalar API reference <a href="/api/v1/docs/reference">here</a> <br>
+`;
+
 export const oapi = openapi(
   "/api/v1/docs/openapi",
   {
     openapi: "3.0.3",
     info: {
       title: "Shorttiee backend",
-      description: "Shorttiee backend documentation",
+      description,
       version: "1.0.0"
     },
     paths: {}
