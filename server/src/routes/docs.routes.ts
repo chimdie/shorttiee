df8526628772dc81da0ee7432d60eb67131a0b19
Micah Effiong <micaiah.effiong@gmail.com>
2024-12-16 16:04:31 +0100

    add scalar static script
    configure endpoint for scalar-ui

--- server/src/routes/docs.routes.ts
+++ server/src/routes/docs.routes.ts
@@ -1,7 +1,30 @@
-import { Router } from "express";
+import express, { Router } from "express";
+import path from "node:path";
 import { oapi } from "../config/docs.config";
+import { apiReference } from "@scalar/express-api-reference";
+import { appEnv } from "../config/env.config";
 
 export const docsRouter = Router();
+
+docsRouter.use("/static", express.static(path.resolve("src/public")));
+
+docsRouter.use("/reference", (req, res) => {
+  apiReference({
+    theme: "bluePlanet",
+    // layout?: 'modern' | 'classic';
+    layout: req.query.layout ?? "modern",
+    isEditable: true,
+
+    darkMode: true,
+    cdn: "/api/v1/docs/static/js/api-reference.js",
+    favicon: "favicon-32x32.png",
+    spec: {
+      url: `http://localhost:${appEnv.PORT}/api/v1/docs/openapi.json`,
+      favicon: "/favicon-32x32.png"
+    }
+  })(req, res);
+});
+
 docsRouter.use(
   oapi.swaggerui({
     deepLinking: true,
