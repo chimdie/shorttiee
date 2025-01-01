fb00a2dc0b885958a0ed172571efd5bb87bcfa0b
sammy-code98 <sammysmart98@yahoo.com>
2025-01-01 22:58:51 +0100

    feat: scalfold landing page - create landing page folder - scaldold project

---
+++ landing/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Adjust this to match your HTML file paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
