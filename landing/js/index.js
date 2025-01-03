f84712c5a8de00efda58326521f52602b0706908
sammy-code98 <sammysmart98@yahoo.com>
2025-01-03 15:21:21 +0100

    feat: add cities linear animation in hero section

--- landing/js/index.js
+++ landing/js/index.js
@@ -5,3 +5,33 @@ navBtn.addEventListener("click", (e) => {
   console.log("clicked", e.target);
   navBar.classList.toggle("invisible");
 });
+
+
+// scrolling text animation
+
+const scrollingText = document.querySelector("#scrollingText");
+
+let scrollSpeed = 50;
+let scrollAmount = 2;
+
+function startScrolling() {
+  const containerWidth =  scrollingText.parentElement.offsetWidth;
+  const textWidth = scrollingText.offsetWidth;
+
+
+  let position = containerWidth;
+
+  function scrollStep(){
+    position -= scrollAmount
+
+    if(position < -textWidth){
+      position = containerWidth
+    }
+    scrollingText.style.transform = `translateX(${position}px)`;
+    requestAnimationFrame(scrollStep)
+  }
+  scrollStep()
+  
+}
+
+  document.addEventListener("DOMContentLoaded", startScrolling);
