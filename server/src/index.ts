6651674474319b33b8f6941b18648929dc025cc6
Micah Effiong <micaiah.effiong@gmail.com>
2025-02-04 16:21:43 +0100

    setup creating initial admin account

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import http from "http";
 import debug from "debug";
 import { app } from "./app";
 import { appEnv } from "./config/env.config";
@@ -6,6 +7,8 @@ import { domainValidator } from "./utils/domain-validator";
 import { OTP } from "./utils/otp";
 import { isAllDBTableMigrated, db } from "./config/db.config";
 import { AppEventEmitter } from "./config/events";
+import assert from "assert";
+import { createAdmin } from "./db/users.db";
 
 new CreateApplicationService(app)
   .addService("otp", OTP)
@@ -18,7 +21,30 @@ if (!isAllDBTableMigrated()) {
   process.exit(1);
 }
 
-const server = app.listen(appEnv.PORT);
+const server = http.createServer(app);
+
+function serverListen(server: http.Server) {
+  server.listen(appEnv.PORT, async () => {
+    const [err] = await createAdmin();
+    if (err) {
+      debug("app:db:createAdmin")(err);
+    }
+  });
+}
+
+serverListen(server);
+
+server.on("error", (e) => {
+  assert("code" in e);
+  if (e.code === "EADDRINUSE") {
+    console.error("Address in use, retrying...");
+    setTimeout(() => {
+      server.close();
+      serverListen(server);
+    }, 1000);
+  }
+});
+
 process.on("SIGTERM", shutDown);
 process.on("SIGINT", shutDown);
 process.on("exit", shutDown);
@@ -27,7 +53,8 @@ server.on("connection", () => {
   // console.log("connection");
 });
 
-function shutDown() {
+function shutDown(code: number) {
+  debug("app:shutDown")("CODE: " + code);
   debug("app:shutDown")("Received kill signal, shutting down gracefully");
 
   server.close(() => {
