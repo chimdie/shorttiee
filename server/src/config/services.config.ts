1a6a78e00ad5ba8bbdad7c0c22fa71acb1f62047
Micah Effiong <micaiah.effiong@gmail.com>
2025-06-22 16:27:49 +0100

    refine types to prevent duplicates

--- server/src/config/services.config.ts
+++ server/src/config/services.config.ts
@@ -1,12 +1,18 @@
-import type { Application } from "express"
+import type { Application } from "express";
+import http from "http";
 
-export class CreateApplicationService {
-  constructor(private app: Application) { }
-  addService<T extends keyof Express.Locals>(name: T, service: Express.Locals[T]) {
-    this.app.locals[name] = service;
-    return this
+export class CreateApplicationService<T extends keyof Express.Locals> {
+  constructor(private app: Application) {}
+  addService<U extends T>(
+    name: U,
+    service: Express.Locals[T]
+  ): Exclude<T, U> extends never
+    ? Omit<CreateApplicationService<any>, "addService">
+    : CreateApplicationService<Exclude<T, U>> {
+    this.app.locals[name as T] = service;
+    return this;
   }
   build() {
-    return this.app
+    return http.createServer(this.app);
   }
 }
